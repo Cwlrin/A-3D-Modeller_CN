cce46b6f5ba0b1f6a7a3121be3ab356309b78def
Cwlrin <20304773@qq.com>
2022-06-14 15:58:19 +0800

    优化代码 / optimize code

--- viewer.py
+++ viewer.py
@@ -1,24 +1,19 @@
+import numpy
 from OpenGL.GL import glCallList, glClear, glClearColor, glColorMaterial, glCullFace, glDepthFunc, glDisable, glEnable, \
     glFlush, glGetFloatv, glLightfv, glLoadIdentity, glMatrixMode, glMultMatrixf, glPopMatrix, glPushMatrix, \
     glTranslated, glViewport, GL_AMBIENT_AND_DIFFUSE, GL_BACK, GL_CULL_FACE, GL_COLOR_BUFFER_BIT, GL_COLOR_MATERIAL, \
     GL_DEPTH_BUFFER_BIT, GL_DEPTH_TEST, GL_FRONT_AND_BACK, GL_LESS, GL_LIGHT0, GL_LIGHTING, GL_MODELVIEW, \
     GL_MODELVIEW_MATRIX, GL_POSITION, GL_PROJECTION, GL_SPOT_DIRECTION
-from OpenGL.constants import GLfloat_3, GLfloat_4
 from OpenGL.GLU import gluPerspective, gluUnProject
 from OpenGL.GLUT import glutCreateWindow, glutDisplayFunc, glutGet, glutInit, glutInitDisplayMode, glutInitWindowSize, \
-    glutMainLoop, GLUT_SINGLE, GLUT_RGB, GLUT_WINDOW_HEIGHT, GLUT_WINDOW_WIDTH, glutCloseFunc
-import numpy
+    glutMainLoop, GLUT_SINGLE, GLUT_RGB, GLUT_WINDOW_HEIGHT, GLUT_WINDOW_WIDTH
+from OpenGL.constants import GLfloat_3, GLfloat_4
 from numpy.linalg import norm, inv
-import random
-from OpenGL.GL import glBegin, glColor3f, glEnd, glEndList, glLineWidth, glNewList, glNormal3f, glVertex3f, GL_COMPILE, \
-    GL_LINES, GL_QUADS
-from OpenGL.GLU import gluDeleteQuadric, gluNewQuadric, gluSphere
 
-import color
-from scene import Scene
-from primitive import init_primitives, G_OBJ_PLANE
-from node import Sphere, Cube, SnowFigure
 from interaction import Interaction
+from node import Sphere, Cube, SnowFigure
+from primitive import init_primitives, G_OBJ_PLANE
+from scene import Scene
 
 
 class Viewer(object):
